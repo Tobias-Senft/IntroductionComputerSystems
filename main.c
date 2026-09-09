ed1a3c9a54a729b1e618ef495e2742590e2b1216
Tobias <Tobiassenft@hotmail.com>
2026-09-09 13:58:13 +0200

    Windows complie

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 //To run (linux/mac): ./main.out example.bmp example_inv.bmp
 
 //To compile (win): gcc cbmp.c main.c -o main.exe -std=c99
-//To run (win): main.exe example.bmp example_inv.bmp
+//To run (win): ./main.exe example.bmp example_inv.bmp
 
 #include <stdlib.h>
 #include <stdio.h>
